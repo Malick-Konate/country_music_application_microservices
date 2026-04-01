7147ca8e21184d92f7f4104dfde0ceaa38c5f818
Malick-Konate <malickzie11@gmail.com>
2026-03-31 22:10:01 -0400

    Migrate database configuration from MySQL to PostgreSQL in application.yaml, build.gradle, and docker-compose.yml; update schema.sql for PostgreSQL compatibility

--- user-service/src/main/resources/schema.sql
+++ user-service/src/main/resources/schema.sql
@@ -2,12 +2,14 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-    id       INT AUTO_INCREMENT PRIMARY KEY,
+    id       serial,
     user_id  VARCHAR(100) NOT NULL UNIQUE,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email    VARCHAR(150) NOT NULL UNIQUE,
     fullname VARCHAR(150) NOT NULL,
     country  VARCHAR(100) NOT NULL,
-    age      INT          NOT NULL
+    age      INT          NOT NULL,
+    PRIMARY KEY (id)
+
 );
